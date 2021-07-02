ba829cb160ca8fb1d6b575ec0ab4e43bf666aa61
georgale <aleksandar_georgiev@starsgroup.com>
2021-07-02 18:38:58 +0300

    Fetch data from Contentful

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,47 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Document } from '@contentful/rich-text-types'
+import { Asset, createClient } from 'contentful'
+import { GetStaticProps } from 'next'
 import styles from '../styles/index.module.scss'
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <div className={styles.body}>
-      Hi, my name is Alexander Georgiev and I am a front end developer
+interface Props {
+  bio: {
+    fields: Asset['fields'] & {
+      helloMessage: Document
+      contactInformation: Document
+    }
+  }
+}
+// TODO: Auto generate types https://github.com/intercom/contentful-typescript-codegen
+export const getStaticProps: GetStaticProps = async () => {
+  const client = createClient({
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    space: process.env.CONTENTFUL_SPACE_ID,
+  })
+
+  const res = await client.getEntries({ content_type: 'bio' })
+
+  return {
+    props: {
+      bio: res.items[0],
+    },
+  }
+}
+
+export const Home = ({ bio }: Props): JSX.Element => {
+  // eslint-disable-next-line no-console
+  console.log(bio)
+
+  return (
+    <div className="container">
+      <div className={styles.body}>
+        {documentToReactComponents(bio.fields.helloMessage)}
+      </div>
+      <div className={styles.test}>
+        {documentToReactComponents(bio.fields.contactInformation)}
+      </div>
     </div>
-    <div className={styles.test}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
-      molestias obcaecati dolor aliquam voluptatibus ea unde neque blanditiis ad
-      eveniet libero at rerum consequuntur porro ab harum, animi nostrum dolore.
-    </div>
-  </div>
-)
+  )
+}
 
 export default Home
