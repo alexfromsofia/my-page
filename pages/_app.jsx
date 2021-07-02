a5727e2ec6e2f2a6110bfe63ccc478d26f2894f3
georgale <aleksandar_georgiev@starsgroup.com>
2021-07-02 16:01:14 +0300

    Add stylelint and sass
    test
    
    test
    
    test

---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import '../styles/normalize.css'
+import '../styles/main.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
